4a4edbceb63ba9f3b052425114baa12a2e28f8ae
mateuszklos37@gmail.com <mateuszklos37@gmail.com>
2020-03-17 21:56:15 +0100

    Initial commit

---
+++ src/test/java/com/kodilla/project/kodillaproject/KodillaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.project.kodillaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
